df4601f51f1a92667f75a5779ffa78fdb51c4e6e
Astrochamp <ivanoconnor@hotmail.co.uk>
2022-02-12 11:00:11 +0000

    Add back removed functionality (oops!)

--- speed/__init__.py
+++ speed/__init__.py
@@ -1,4 +1,12 @@
 def checkSpeed(func, r):
+    import os, sys
+    class noPrint:
+        def __enter__(self):
+            self._original_stdout = sys.stdout
+            sys.stdout = open(os.devnull, 'w')
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            sys.stdout.close()
+            sys.stdout = self._original_stdout
     from time import perf_counter as pf
     start = pf()
     with noPrint():
